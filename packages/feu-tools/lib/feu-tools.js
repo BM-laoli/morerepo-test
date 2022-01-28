248d0eea30c63a785e3419eafd64831074b6edd0
joney <joney.s.li@newegg.com>
2022-01-28 16:09:05 +0800

    fix:Publish Filad

--- packages/feu-tools/lib/feu-tools.js
+++ packages/feu-tools/lib/feu-tools.js
@@ -9,4 +9,10 @@ function min(a, b) {
     return Math.min(a, b);
 }
 
+// 我现在再来发布一下 发布失败之后没有 重发布的配置选项了 
+function test555(cb1,bc2)  {
+  console.log("--->", cb1(), bc2())
+  return 6666
+}
+
 module.exports = { add, min };
